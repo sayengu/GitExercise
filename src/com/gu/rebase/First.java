93b6bd250af4e2d61c533ba96cc6c6873b5689b8
gsy <gusy@akulaku.com>
2018-09-15 15:01:14 +0800

    master modify

--- src/com/gu/rebase/First.java
+++ src/com/gu/rebase/First.java
@@ -2,6 +2,7 @@ package com.gu.rebase;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello github!");
+        System.out.println("Hello change!");
+        System.out.println("this line will not be conflict");
     }
 }
