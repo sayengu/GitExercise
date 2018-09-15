707882850911cff82438bd370679a1ea55e49f90
gsy <gusy@akulaku.com>
2018-09-15 14:57:44 +0800

    create branch to rebase

--- src/com/gu/rebase/First.java
+++ src/com/gu/rebase/First.java
@@ -2,6 +2,6 @@ package com.gu.rebase;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello github!");
+        System.out.println("git branch!");
     }
 }
