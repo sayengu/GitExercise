487634ea20e4245aa60964fa582496af82608cc4
gsy <gusy@akulaku.com>
2018-09-15 15:08:27 +0800

    add loop

--- src/com/gu/rebase/First.java
+++ src/com/gu/rebase/First.java
@@ -2,6 +2,9 @@ package com.gu.rebase;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("git branch!");
+        System.out.println("git rebase!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("now i=" + i);
+        }
     }
 }
