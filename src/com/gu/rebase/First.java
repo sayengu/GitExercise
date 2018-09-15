d89c7e1ac2be6c8dd35a9ec4a8aa915051cb26b4
gsy <gusy@akulaku.com>
2018-09-15 15:03:39 +0800

    branch merge

--- src/com/gu/rebase/First.java
+++ src/com/gu/rebase/First.java
@@ -2,7 +2,7 @@ package com.gu.rebase;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello change!");
-        System.out.println("this line will not be conflict");
+        System.out.println("Hello merge!");
+        System.out.println("Does the commit message disappear?");
     }
 }
