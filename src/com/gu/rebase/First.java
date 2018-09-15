6ff2aa0c7bb872031fcc5fedda7852c84c829304
gsy <gusy@akulaku.com>
2018-09-15 15:20:48 +0800

    lamda & param Function

--- src/com/gu/rebase/First.java
+++ src/com/gu/rebase/First.java
@@ -1,7 +1,20 @@
 package com.gu.rebase;
 
 public class First {
+    @FunctionalInterface
+    public interface FunctionParamInterface {
+        int apply() throws Exception;
+    }
+
+
     public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        try {
+            System.out.println(((FunctionParamInterface) () -> a + b).apply());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         System.out.println("Hello change!");
         System.out.println("this line will not be conflict");
     }
